b90661d20be43f31eb07da3bfbd73940027ef1b2
toly <tolytrader@gmail.com>
2016-09-10 22:15:48 +0300

    add api - start course + active courses

--- server/app/views.py
+++ server/app/views.py
@@ -6,13 +6,26 @@ from .models import Course, Student, StudentCourse
 
 from .serializers import CourseSerializer
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_queryset(self):
+        result = super(CourseViewSet, self).get_queryset()
+
+        username = self.request.GET.get('username', None)
+        active = self.request.GET.get('active', None)
+
+        if not username or active != '1':
+            return result
+
+        user = Student.objects.get(nickname=username)
+        courses_ids = StudentCourse.objects.filter(student=user, active=True).values_list('course_id', flat=True)
+        return result.filter(id__in=courses_ids)
 
     @detail_route(methods=['GET'])
     def start(self, request, pk=None):
