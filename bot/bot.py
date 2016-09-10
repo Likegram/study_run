60e29f0a471997f109b40e2d621e8138922c27f6
BlackTrub <lightlocdog@gmail.com>
2016-09-10 21:02:53 +0300

    add commands;

--- bot/bot.py
+++ bot/bot.py
@@ -2,12 +2,19 @@
 import config
 import telebot
 
+from contains import START_MESSAGE
+
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    bot.send_message(message.chat.id, START_MESSAGE)
+
+@bot.message_handler(commands=['course'])
+def command_get_course(message):
+    course = None
+    bot.send_message(message.chat.id, course)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
