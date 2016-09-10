79c95fa5fe201b2cd1ceccb623e9c53cc755fa6b
BlackTrub <lightlocdog@gmail.com>
2016-09-10 21:59:34 +0300

    process;

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
-import config
 import telebot
+import requests
 
+from config import token, URL_SERVER
 from contains import START_MESSAGE
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(token)
+
+
+def do_api(url, params):
+    data = requests.get(URL_SERVER + url, params)
+    return data.json()
+
+
+def format_text_from_json(data):
+    text = '%s %s' % (data[0]['title'], data[0]['description'])
+    return text
 
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
     bot.send_message(message.chat.id, START_MESSAGE)
 
+
 @bot.message_handler(commands=['course'])
 def command_get_course(message):
-    course = None
-    bot.send_message(message.chat.id, course)
+    course = do_api('/api/course/', {'username': message.chat.username})
+    text_message = format_text_from_json(course)
+    bot.send_message(message.chat.id, text_message)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from token import TOKEN_FOR_BOT
 
+URL_SERVER = 'http://127.0.0.1:8000'
+
 token = TOKEN_FOR_BOT
