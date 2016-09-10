cc9ea20ebdf1bfc335e74552fc26d4f49ea16b3d
BlackTrub <lightlocdog@gmail.com>
2016-09-10 22:32:29 +0300

    process bot;

--- bot/bot.py
+++ bot/bot.py
@@ -15,7 +15,10 @@ def do_api(url, params):
 
 
 def format_text_from_json(data):
-    text = '%s %s' % (data[0]['title'], data[0]['description'])
+    try:
+        text = '<b>%s</b> %s' % (data[0]['title'], data[0]['description'])
+    except:
+        text = '<b> not </b> text'
     return text
 
 
@@ -28,7 +31,8 @@ def command_start(message):
 def command_get_course(message):
     course = do_api('/api/course/', {'username': message.chat.username})
     text_message = format_text_from_json(course)
-    bot.send_message(message.chat.id, text_message)
+    print text_message
+    bot.send_message(message.chat.id, text_message, parse_mode='HTML')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
