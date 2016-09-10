777fd430c8fcd1fadb7012819a5329ad4990506e
BlackTrub <lightlocdog@gmail.com>
2016-09-10 23:16:34 +0300

    process bot;

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@
 import requests
 
 import telebot
+from telebot import types
 
 from config import token, URL_SERVER
 from contains import START_MESSAGE
@@ -16,7 +17,7 @@ def do_api(url, params):
 
 def format_text_from_json(data):
     try:
-        text = '<b>%s</b> %s' % (data[0]['title'], data[0]['description'])
+        text = '<b>%s</b> \n %s' % (data[0]['title'], data[0]['description'])
     except:
         text = '<b> not </b> text'
     return text
@@ -24,14 +25,19 @@ def format_text_from_json(data):
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
-    bot.send_message(message.chat.id, START_MESSAGE)
+    print message
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Курсы')
+    markup.row('Мои курсы')
+    print markup.row_width
+    bot.send_message(message.chat.id, START_MESSAGE, reply_markup='/course')
 
 
 @bot.message_handler(commands=['course'])
 def command_get_course(message):
     course = do_api('/api/course/', {'username': message.chat.username})
     text_message = format_text_from_json(course)
-    print text_message
+
     bot.send_message(message.chat.id, text_message, parse_mode='HTML')
 
 if __name__ == '__main__':
