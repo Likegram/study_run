51416ba42c77ef7f1620bdc5a5248583edcad28a
toly <tolytrader@gmail.com>
2016-09-10 22:03:46 +0300

    add start course method

--- server/app/urls.py
+++ server/app/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from .routers import router
 
+# from .views import start_course
+
 
 urlpatterns = [
 
     url(r'^', include(router.urls)),
+    # url(r'^api/course/start/$', start_course),
 
 ]
